3ff8b75933eb2810798c83361e6bc6aa27765db7
bung87 <crc32@qq.com>
2026-02-28 21:06:33 +0800

    Remove try-catch blocks from test cases
    Let exceptions propagate naturally to test framework for proper failure reporting

--- tests/test_locale.nim
+++ tests/test_locale.nim
@@ -17,29 +17,15 @@ suite "Locale Tests":
       check localeName.toLowerAscii() == localeName
   
 
-  test "loadCfgLocaleData loads configuration":
-    var manager: TLocaleManager
-    try:
-      manager.loadCfgLocaleData("LocaleData.cfg")
-      check true
-    except:
-      # File might not exist in test environment
-      echo "Config file not found, skipping test"
       
   test "getKeyLang retrieves localized string":
     var manager: TLocaleManager
-    try:
-      manager.loadCfgLocaleData("LocaleData.cfg")
-      let hello = manager.getKeyLang("Hello", "de")
-      check hello.len > 0
-    except:
-      echo "Config file not found, skipping test"
+    manager.loadCfgLocaleData("LocaleData.cfg")
+    let hello = manager.getKeyLang("Hello", "de")
+    check hello.len > 0
       
   test "getKey retrieves localized string for current locale":
     var manager: TLocaleManager
-    try:
-      manager.loadCfgLocaleData("LocaleData.cfg")
-      let hello = manager.getKey("Hello")
-      check hello.len > 0
-    except:
-      echo "Config file not found, skipping test"
+    manager.loadCfgLocaleData("LocaleData.cfg")
+    let hello = manager.getKey("Hello")
+    check hello.len > 0
